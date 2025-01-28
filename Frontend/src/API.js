c968d942fed09d052f7502f56182eacdd9c6a222
illiteratewriter <manukoshyabraham@gmail.com>
2025-01-28 23:43:34 +0530

    fix: update frontend url to aws url

--- Frontend/src/API.js
+++ Frontend/src/API.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api', // Replace with your base URL
+  baseURL: "http://3.87.8.180:5002/api", // Replace with your base URL
 });
 
 export default api;
