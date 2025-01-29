738dd5339c45804cedcd022ca0e0a5f7517eaa0b
illiteratewriter <manukoshyabraham@gmail.com>
2025-01-29 22:43:02 +0530

    chore: change base url to cloudfront

--- Frontend/src/API.js
+++ Frontend/src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.87.8.180:5002/api", // Replace with your base URL
+  baseURL: "https://d13fbrqampg8cm.cloudfront.net/api", // Replace with your base URL
 });
 
 export default api;
